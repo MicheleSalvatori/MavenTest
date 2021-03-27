3e86abfde6529f56f81ab4404b2093122e88fcc7
Michele <michelesalvv@gmail.com>
2021-03-27 17:17:39 +0100

    init project

---
+++ src/main/java/michele/test_maven/App.java
@@ -0,0 +1,13 @@
+package michele.test_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

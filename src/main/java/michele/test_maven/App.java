0b80679f577db887266d7accb41f97d31c940ea7
Michele <michelesalvv@gmail.com>
2021-03-27 19:13:50 +0100

    edit

--- src/main/java/michele/test_maven/App.java
+++ src/main/java/michele/test_maven/App.java
@@ -1,5 +1,7 @@
 package michele.test_maven;
 
+import java.util.logging.Level;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	java.util.logging.Logger.getGlobal().log(Level.INFO, "test");
     }
 }
